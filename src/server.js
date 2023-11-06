de75e7f5f8c0c09a7760664fead82aff5e6c6320
Hoi Won KIM <gw31513@gmail.com>
2023-11-07 12:44:07 +1300

    FrontEnd Setup

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
 import express from "express";
+import path from "path";
 
 const app = express();
 
-console.log("hello");
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "/views")); // 'views' 디렉토리의 정확한 위치를 지정합니다.
+app.use("/public", express.static(path.join(__dirname, "/public"))); // 정적 파일 경로를 추가.
+app.get("/", (req, res) => res.render("home")); // 'home'은 views 폴더 내에 있는 home.pug를 가리킵니다.
 
-app.listen(3000);
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+app.listen(3000, handleListen);

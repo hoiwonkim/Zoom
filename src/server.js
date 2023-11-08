9da74ad0d3c59d577c8df33e6a3b4122a376eb14
Hoi Won KIM <gw31513@gmail.com>
2023-11-08 13:21:24 +1300

    Websocket Event

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 import path from "path";
 
@@ -6,7 +8,17 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "/views")); // 'views' 디렉토리의 정확한 위치를 지정합니다.
 app.use("/public", express.static(path.join(__dirname, "/public"))); // 정적 파일 경로를 추가.
-app.get("/", (req, res) => res.render("home")); // 'home'은 views 폴더 내에 있는 home.pug를 가리킵니다.
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+function handleConnection(socket) {
+    console.log(socket);
+  }
+  wss.on("connection", handleConnection);
+ 
+server.listen(3000, handleListen);

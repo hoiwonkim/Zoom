45c31608df97d9d792cdb51385c8a8f7789f27b9
Hoi Won KIM <gw31513@gmail.com>
2023-11-09 14:13:45 +1300

    WebSocket Message Refactory

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,17 +1,19 @@
 const socket = new WebSocket(`ws://${window.location.host}`);
 
-socket.addEventListener("open", () => {
-    console.log("Connected to Server ✅");
-  });
-  
-  socket.addEventListener("message", (message) => {
-    console.log("New message: ", message.data);
-  });
-  
-  socket.addEventListener("close", () => {
-    console.log("Disconnected from Server ❌");
-  });
-  
-  setTimeout(() => {
-    socket.send("hello from the browser!");
-  }, 10000);
+function handleOpen() {
+  console.log("Connected to Server ✅");
+}
+
+socket.addEventListener("open", handleOpen);
+
+socket.addEventListener("message", (message) => {
+  console.log("New message: ", message.data);
+});
+
+socket.addEventListener("close", () => {
+  console.log("Disconnected from Server ❌");
+});
+
+setTimeout(() => {
+  socket.send("hello from the browser!");
+}, 10000);
